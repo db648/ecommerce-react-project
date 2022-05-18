f991e35ce5bda2866163d543fcec3bb0ffd41d34
db648 <95957492+db648@users.noreply.github.com>
2022-05-18 21:39:39 +0530

    some changes

--- practice.js
+++ practice.js
@@ -1,31 +1,35 @@
-var capitals = [{ // creating an object 'capital' with
-    Burma: "Naypyitaw" // key "Burma" and value "Naypitaw"
-}];
+// var capitals = [{ // creating an object 'capital' with
+//     Burma: "Naypyitaw" // key "Burma" and value "Naypitaw"
+// }];
 
-console.log(capitals);
+// console.log(capitals);
 
-//function rename() { // function to rename on button click
-    capitals = capitals.map(function(obj) {
-        obj['Myanmar'] = obj['Burma']; // Assign new key
-        delete obj['Burma']; // Delete old key
-        return obj;
-    });
-    console.log(capitals);
+// //function rename() { // function to rename on button click
+//     capitals = capitals.map(function(obj) {
+//         obj['Myanmar'] = obj['Burma']; // Assign new key
+//         delete obj['Burma']; // Delete old key
+//         return obj;
+//     });
+//     console.log(capitals);
+// // }
+
+// // rename()
+
+// let obj = {
+//     name : "durga"
 // }
 
-// rename()
+// // obj["firstname"] = obj["name"]
+// // delete obj["name"]
+// // console.log(obj)
 
-let obj = {
-    name : "durga"
-}
+// let obj2 = obj
 
-// obj["firstname"] = obj["name"]
-// delete obj["name"]
-// console.log(obj)
+// obj2.name = "bhavani"
 
-let obj2 = obj
+// console.log("obj",obj)
+// console.log("obj2",obj2)
 
-obj2.name = "bhavani"
-
-console.log("obj",obj)
-console.log("obj2",obj2)
+for (var i = 0; i < 4; i++){ 
+    console.log(i);
+    }
